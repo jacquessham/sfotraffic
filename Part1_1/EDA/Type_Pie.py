7421224de766ca563eabd573a8d7766eedf13efb
Jacques Sham <jacquessham2015@gmail.com>
2021-03-29 22:59:04 -0700

    Update comments

--- Part1_1/EDA/Type_Pie.py
+++ Part1_1/EDA/Type_Pie.py
@@ -9,17 +9,18 @@ init_notebook_mode(connected=True)
 # Use monthly data
 df = pd.read_csv('../../Data/sfopax_eda.csv')
 
+# Get sum by type (Depart/Arrival)
 df = df.groupby('type').sum().reset_index()
+# Change from technical terms to common use terms
 df.replace({'Deplaned':'Arrival', 'Enplaned':'Departure',
 	            'Thru / Transit':'Transit'})
-print(df)
 
+# Plot Graph
 data = [go.Pie(labels=df['type'],values=df['pax_count'],
 	           marker=dict(colors=['rgb(0, 204, 102)',
 	          	                  'rgb(255, 153, 91)',
 	          	                  'rgb(102, 178, 255)']))]
 layout = dict(title='Percentage of Airplane Activities')
-
 fig = go.Figure(data=data, layout=layout)
 
 plotly.offline.plot(fig, filename='type_pie.html')
