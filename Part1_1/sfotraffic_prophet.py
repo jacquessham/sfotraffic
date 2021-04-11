59f247e2f54d413801e06aaed4efb07b70f681af
Jacques Sham <jacquessham2015@gmail.com>
2021-04-11 16:15:26 -0700

    Fix bug, now able to predict more than 24 months

--- Part1_1/sfotraffic_prophet.py
+++ Part1_1/sfotraffic_prophet.py
@@ -4,40 +4,48 @@ from prophet import Prophet
 
 
 def result_gam(pred_period=24):
-	# Use monthly data
-	df = pd.read_csv('../Data/sfopax_month.csv')
-	# Reformat data frame
-	df['date'] = pd.to_datetime(df['date'], format='%Y-%m-%d')
-	df['year'] = df['date'].dt.year
-	df['pax_count'] = df['pax_count']/1000000
+    # Use monthly data
+    df = pd.read_csv('../Data/sfopax_month.csv')
+    # Reformat data frame
+    df['date'] = pd.to_datetime(df['date'], format='%Y-%m-%d')
+    df['year'] = df['date'].dt.year
+    df['pax_count'] = df['pax_count']/1000000
 
-	X_train = df[df['year']<=2015][['date','pax_count']]
-	X_test = df[df['year']>2015][['date','pax_count']]
-	# Save the dataframe for return object
-	X_train_org = X_train
-	X_test_org = X_test
-	# Rename the column names for Prophet
-	X_train = X_train.rename(columns={'date':'ds', 'pax_count':'y'})
-	X_test = X_test.rename(columns={'date':'ds', 'pax_count':'y'})
+    X_train = df[df['year']<=2015][['date','pax_count']]
+    X_test = df[df['year']>2015][['date','pax_count']]
 
-	# Train the model and predict
-	model = Prophet()
-	model.fit(X_train)
-	yhat = model.predict(X_test)
+    
+    # Save the dataframe for return object
+    X_train_org = X_train
+    X_test_org = X_test
+    # Prepare date list for data frame if pred_period != 24
+    if pred_period > 24:    
+        X_test_startdate = X_train['date'].max()+ pd.tseries.offsets.MonthEnd(1)
+        X_test_enddate = X_train['date'].max()+ pd.tseries.offsets.MonthEnd(pred_period)
+        X_test_date = pd.date_range(X_test_startdate, X_test_enddate, freq='m')
+        X_test['test'] = X_test_date
+    # Rename the column names for Prophet
+    X_train = X_train.rename(columns={'date':'ds', 'pax_count':'y'})
+    X_test = X_test.rename(columns={'date':'ds', 'pax_count':'y'})
 
-	# Get RMSE
-	fb_rmse = mse(X_test['y'], yhat['yhat'].tolist()[:24])**0.5
+    # Train the model and predict
+    model = Prophet()
+    model.fit(X_train)
+    yhat = model.predict(X_test)
 
-	# Reformat the data frame for result
-	yhat = yhat[['ds','yhat']]
-	yhat = yhat.rename(columns={'ds':'date', 'yhat':'pred'})
+    # Get RMSE
+    fb_rmse = mse(X_test['y'], yhat['yhat'].tolist()[:24])**0.5
 
-	# Save result and return
-	result = {}
-	result['X_train'] = X_train_org
-	result['X_test'] = X_test_org
-	result['pred'] = yhat
-	result['rmse'] = fb_rmse
+    # Reformat the data frame for result
+    yhat = yhat[['ds','yhat']]
+    yhat = yhat.rename(columns={'ds':'date', 'yhat':'pred'})
 
-	return result
+    # Save result and return
+    result = {}
+    result['X_train'] = X_train_org
+    result['X_test'] = X_test_org
+    result['pred'] = yhat
+    result['rmse'] = fb_rmse
+
+    return result
 
