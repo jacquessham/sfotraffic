8f18b24123dc41f96f5c7dbee36b015de6f09fea
Jacques Sham <jacquessham2015@gmail.com>
2023-03-11 17:17:03 -0800

    Upload the completed code

--- Part2_2/pred_prod.py
+++ Part2_2/pred_prod.py
@@ -3,14 +3,14 @@ from pandas.tseries.offsets import MonthEnd
 from statsmodels.tsa.holtwinters import ExponentialSmoothing
 
 
-def result_hw(df, model=None, pred_period=24):
+def result_hw(df, model=None, pred_period=24,trend_mode='mul',seasonal_mode='mul'):
     # The stats model cannot handle huge number, must divide by 1M
-    def model_training(df):
-        model = ExponentialSmoothing(df['pax_count'], trend='mul',
-                                     seasonal='mul', seasonal_periods=12).fit()
+    def model_training(df,trend_mode,seasonal_mode):
+        model = ExponentialSmoothing(df['pax_count'], trend=trend_mode,
+                                     seasonal=seasonal_mode, seasonal_periods=12).fit()
         return model
     if model is None:
-        model = model_training(df)
+        model = model_training(df,trend_mode,seasonal_mode)
     pred_startdate = df['date'].max()+ pd.tseries.offsets.MonthEnd(1)
     pred_enddate = df['date'].max()+ pd.tseries.offsets.MonthEnd(pred_period)
     pred_date = pd.date_range(pred_startdate, pred_enddate, freq='m')
@@ -19,5 +19,36 @@ def result_hw(df, model=None, pred_period=24):
 
     return model, pred
 
-def recovery_path(df, shock_bottom, recovery_duration):
-    return
+def recovery_path(df, pred, shock_date, recovery_reference, recovery_duration, seasonality_mod=1):
+    # Calculate Moving Average
+    df['MA'] = df['pax_count'].rolling(12).mean()
+    
+    # Set the data on recover_reference as index = 100 and calculate the index
+    paxindex_base = df[df['date'].dt.date==recovery_reference]['pax_count'].tolist()[0]
+    df['paxindex'] = df['pax_count']/paxindex_base*100
+    maindex_base = df[df['date'].dt.date==recovery_reference]['MA'].tolist()[0]
+    df['maindex'] = df['MA']/maindex_base*100
+    
+    # Obtain the diff between index and moving average index
+    df['diffindex'] = df['paxindex'] - df['maindex']
+
+    # Find the magitude of each unit of index
+    pax_recovered = pred.head(1)['pred'].tolist()[0]
+    index_recovered = df[df['date'].dt.date==recovery_reference+MonthEnd(recovery_duration)]['maindex'].tolist()[0]
+    pax_shock = df[df['date'].dt.date==shock_date]['pax_count'].tolist()[0]
+    pax_per_index = (pax_recovered-pax_shock)/(index_recovered-100)
+
+    # Segment the Recover pred_year
+    df_index_range = df[(df['date'].dt.date>recovery_reference) & (df['date'].dt.date<=(recovery_reference+MonthEnd(recovery_duration)))]
+    # Prepare Prediction Dataframe
+    pred_recovery = pd.DataFrame(df_index_range[['maindex','diffindex']])
+    pred_recovery['date'] = pd.date_range(start=shock_date, periods=recovery_duration, freq='M')
+    pred_recovery = pred_recovery.iloc[1:-1,:]
+    pred_recovery['year'] = pred_recovery['date'].dt.year
+    pred_recovery['month'] = pred_recovery['date'].dt.month
+    # Calculate the predicted trend pax count
+    pred_recovery['raw_prediction'] = pax_shock+(pred_recovery['maindex']-100)*pax_per_index
+    # Calculate the final prediction by adjusting with seasonality
+    pred_recovery['pred'] = pred_recovery['raw_prediction']*(1+(pred_recovery['diffindex']*seasonality_mod/100))
+
+    return pred_recovery[['date','pred']]
--- Part2_2/sfo_pred.py
+++ Part2_2/sfo_pred.py
@@ -1,8 +1,9 @@
+import math
 import json
 import datetime
 from pandas.tseries.offsets import MonthEnd
 from etl_prod import *
-from prod_prod import *
+from pred_prod import *
 from viz_prod import *
 
 
@@ -12,30 +13,41 @@ metadata = json.load(f)
 f.close()
 
 # Check shock_date format is right
-# https://docs.python.org/3/library/datetime.html#datetime.date.fromisoformat
+## https://docs.python.org/3/library/datetime.html#datetime.date.fromisoformat
 try:
-	shock_date = datetime.date.fromisoformat(metadata['shock_date']) + MonthEnd(0)
+    shock_date = datetime.date.fromisoformat(metadata['shock_date']) + MonthEnd(0)
+    recovery_reference = datetime.date.fromisoformat(metadata['recovery_reference'])
 except:
-	print('Date format incorrect, please revise!')
-	quit()
+    print('Date format incorrect, please revise!')
+    quit()
 
 # Data Cleansing and Aggregation
 df = etl(metadata['file'])
 
 # Data before shock
-df_b4shock = df[df['date']<=shock_date]
-print(df_b4shock)
+normality_before_shock = shock_date - MonthEnd(int(metadata['normality_before_shock']))
+df_b4shock = df[df['date']<=normality_before_shock]
 
+# Make prediction (After Recovery)
+model, pred = result_hw(df_b4shock,pred_period=metadata['pred_period']+12,
+                        trend_mode=metadata['trend_mode'],
+                        seasonal_mode=metadata['seasonal_mode'])
 
-# Make prediction
-model, pred = result_hw(df_b4shock,pred_period=metadata['pred_period']+12)
-print(pred)
+## Adjust prediction date
+adjust_seasonality = metadata['shock_recovery']%12
+adjust_year = metadata['shock_recovery']//12
+pred['date'] = pred['date'] + MonthEnd(adjust_year*12)
+pred = pred.iloc[adjust_seasonality:adjust_seasonality+metadata['pred_period'],:]
 
-"""
+# Predict Recovery path
+pred_recovery = recovery_path(df, pred, shock_date, recovery_reference, metadata['shock_recovery'], metadata['seasonality_mod'])
+
 # Vizualize the prediction
-viz(df, pred)
+if metadata['diff_pred']:
+    viz_diff(df[df['date']<=shock_date], pred_recovery, pred)
+else:
+    viz(df[df['date']<=shock_date], pd.concat([pred_recovery, pred]))
 
 # Export Prediction in CSV
 if metadata['result_export_csv']:
-	pred.to_csv('prediction_result.csv',index=False)
-"""
+    pd.concat([pred_recovery, pred]).to_csv('prediction_result.csv',index=False)
