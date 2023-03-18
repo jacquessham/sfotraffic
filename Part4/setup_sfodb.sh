c133990812ef3ff015c9da2c632d50000eb4b60c
Jacques Sham <jacquessham2015@gmail.com>
2023-03-17 21:17:14 -0700

    Upload scripts for src_ tables pipeline

--- Part4/setup_sfodb.sh
+++ Part4/setup_sfodb.sh
@@ -1,2 +1,6 @@
 export PGPASSWORD='sfo'
-psql -h localhost -p 8731 -U sfo -d sfo -a -f ELT/create_table.sql
+psql -h localhost -p 8731 -U sfo -d sfo -a -f ELT/create_tables.sql
+cd ELT
+python upload_data.py
+psql -h localhost -p 8731 -U sfo -d sfo -a -f src_cleansing.sql
+cd ..
